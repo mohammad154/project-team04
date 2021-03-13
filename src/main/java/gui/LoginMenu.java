5f203500fa86e0dc4c8fdf70ce29ed44b618eddd
mhas1381 <mhas.software@gmail.com>
2021-03-13 14:57:26 +0330

    debugging

--- src/main/java/gui/LoginMenu.java
+++ src/main/java/gui/LoginMenu.java
@@ -198,7 +198,7 @@ public class LoginMenu extends javax.swing.JFrame {
 
     /**
      * @param args the command line arguments
-     */
+     */}}
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
